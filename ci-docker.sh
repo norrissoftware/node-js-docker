e0e717892d0e9e7bdaf58352162a18d23e94b0c5
Jeff Norris <norrisjp@gmail.com>
2016-05-19 13:40:38 -0700

    added i and rm

--- ci-docker.sh
+++ ci-docker.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 sudo docker build -t snapci/node-docker-example .
-sudo docker run snapci/node-docker-example
+sudo docker run -i -rm -name bob snapci/node-docker-example whoami
