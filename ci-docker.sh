4643a50112856a1e309879ff3e159ecc103ae1a0
Jeff Norris <norrisjp@gmail.com>
2016-05-19 13:46:49 -0700

    Update ci-docker.sh

--- ci-docker.sh
+++ ci-docker.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 sudo docker build -t snapci/node-docker-example .
-sudo docker run --rm --name bob snapci/node-docker-example whoami
+sudo docker run --rm snapci/node-docker-example whoami
